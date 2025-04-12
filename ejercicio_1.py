7beb5aefa4222bfc85649d881b90176726aabb89
Martín Fernández <martinffg@gmail.com>
2025-04-11 21:01:16 -0300

    update 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,1 +1,14 @@
 ## Escribe un programa que intente dividir dos números. Si el segundo número es cero, captura la excepción ZeroDivisionError y muestra un mensaje de error al usuario.
+
+numero = 10
+nummero2 = 0
+
+try:
+    numero3 = numero / nummero2
+    print(f"Se dividió correctamente.")
+except ZeroDivisionError:
+    print(f"Error de división por cero.")
+else:
+    print(numero3)
+finally:
+    print("Fin del programa.")
