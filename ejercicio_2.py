222f2e2bbbf622c8933b9bc978eb28fd8d15ae34
Martín Fernández <martinffg@gmail.com>
2025-04-11 21:04:25 -0300

    update 2

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,1 +1,15 @@
 ## Escribe un programa que intente sumar un número y una cadena. Si se produce un error de tipo, captura la excepción TypeError y muestra un mensaje de error al usuario.
+
+
+numero = 10
+cadena = "Hola Mundo"
+
+try:
+    suma = numero + cadena
+    print(f"Se sumó correctamente.")
+except TypeError:
+    print(f"Error de tipo, verificar valores ingresados.")
+else:
+    print(suma)
+finally:
+    print("Fin del programa.")
