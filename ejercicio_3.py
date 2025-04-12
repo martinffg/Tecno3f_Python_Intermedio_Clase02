ba6060eed046f97fba4d29485a0eee9e70ca3b37
Martín Fernández <martinffg@gmail.com>
2025-04-11 21:21:24 -0300

    update 3

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,1 +1,14 @@
 ## Escribe un programa que intente acceder a una clave que no existe en un diccionario. Si se produce una excepción KeyError, captura la excepción y muestra
+
+diccionario = {"a": "1", "b": "2", "c": "3"}
+clave = "d"
+
+try:
+    valor = diccionario[clave]
+    print(f"Se calculó el valor de la clave {clave} correctamente.")
+except KeyError:
+    print(f"Error accediendo a una clave inexistente en el Diccionario.")
+else:
+    print(valor)
+finally:
+    print("Fin del programa.")
